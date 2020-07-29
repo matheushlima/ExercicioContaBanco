849b434a40402cc3e905fbfce0d4dcb284b94102
Matheus Lima <math.henry04@hotmail.com>
2020-07-29 00:32:05 -0300

    Add project files.

---
+++ ExerFixCap05/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerFixCap05
+{
+    class Conta
+    {
+        public int NumConta { get; private set; }
+        public string Nome { get; private set; }
+        public double Deposito { get; private set; }
+
+        
+
+        public Conta(int conta, string nome)
+        {
+            NumConta = conta;
+            Nome = nome;
+        }
+
+        public Conta(int conta, string nome , double deposito)
+        {
+            NumConta = conta;
+            Nome = nome;
+            Deposito = deposito;
+        }
+
+        public void VlDeposito(double deposito)
+        {
+            Deposito += deposito;
+        }
+
+        public void Vlsaque(double saque)
+        {
+            Deposito = Deposito - (saque + 5);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumConta}, Titular: {Nome}, Saldo: $ {Deposito.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExerFixCap05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerFixCap05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposito;
+            Conta conbancaria;
+
+            Console.Write("Entre com o núemro de conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Havera deposito inicial (S/N): ");
+            string sn = Console.ReadLine();
+
+            if (sn == "S" || sn == "s")
+            {
+                Console.Write("Entre com o valor de deposito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conbancaria = new Conta(conta, nome, deposito);
+            }
+            else {
+                conbancaria = new Conta(conta, nome);
+            }
+
+            Console.WriteLine($"\nDados da conta: " + conbancaria);
+
+            Console.Write("\nEntre com um valr para deposito: ");
+            double vldeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conbancaria.VlDeposito(vldeposito);
+
+            Console.WriteLine($"Dados atualizados: " + conbancaria);
+
+            Console.Write("\nEntre com um valor para saque: ");
+            double vlsaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conbancaria.Vlsaque(vlsaque);
+
+            Console.WriteLine($"Dados atualizados: " + conbancaria);
+        }
+    }
+}
